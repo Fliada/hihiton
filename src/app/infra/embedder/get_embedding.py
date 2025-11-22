3a76b3f372efbe6ab0147c8db61a20f9a8aa45a2
Симонова Ксения <simonova278@gmail.com>
2025-11-23 01:31:55 +0500

    добавлен эмбеддер

--- src/app/infra/embedder/get_embedding.py
+++ src/app/infra/embedder/get_embedding.py
@@ -1,2 +1,16 @@
 import requests
+from os import getenv
 from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_embedding(text: str):
+
+    response = requests.post(getenv("EMBEDDER_URL"), json={"text": text})
+
+    if response.status_code == 200:
+        data = response.json()
+        embedding = data.get("embedding")
+        return embedding
+    else:
+        print(f"Ошибка: {response.status_code}, {response.text}")
